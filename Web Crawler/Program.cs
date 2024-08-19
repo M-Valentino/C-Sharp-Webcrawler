e774accb23e989bad855e56fd30985d21fbe2aba
Mark Valentino <mike9377mike9377@gmail.com>
2024-08-19 14:22:36 -0400

    first commit

---
+++ Web Crawler/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Web_Crawler
+{
+    static async Task Crawl(string url)
+    {
+        Console.WriteLine(await Fetch(url));
+    } 
+    
+    static async Task<string> Fetch(string url)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            try
+            {
+                string htmlContent = await client.GetStringAsync(url);
+                return htmlContent;
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"Request error: {e.Message}");
+                return null;
+            }
+        }
+    }
+    
+    
+    static async Task Main(string[] args)
+    {
+        await Crawl("https://sacred.neocities.org");
+    }
+}
